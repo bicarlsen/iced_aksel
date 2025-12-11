7e5d66a827c22521f7ae3daeabb74ff22072c152
MellquistD <dennis.mellquist@gmail.com>
2025-12-11 10:08:46 +0100

    Simplest example

--- examples/axes/src/main.rs
+++ examples/axes/src/main.rs
@@ -4,7 +4,12 @@ use std::f64::consts::PI;
 
 use aksel::{PlotPoint, scale::Linear};
 use iced::{Color, Element, Task, Theme};
-use iced_aksel::{Axis, Chart, DragDelta, State, axis, plot::PlotData, shape::Polygon};
+use iced_aksel::{
+    Axis, Chart, DragDelta, State,
+    axis::{self, GridLine, Label},
+    plot::PlotData,
+    shape::Polygon,
+};
 use rand::Rng;
 
 const X_ID: &str = "linear_x";
@@ -25,38 +30,83 @@ struct ExampleApp {
 }
 
 #[derive(Debug, Clone)]
-enum Message {
-    ChartDragged(DragDelta),
-}
+enum Message {}
 
 impl ExampleApp {
     fn init() -> (Self, Task<Message>) {
-        let mut chart_state = State::new()
-            .with_axis(
-                "x_axis_id",
-                Axis::new(Linear::new(0.0, 100.0), axis::Position::Bottom),
-            )
-            .with_axis(
-                "y_axis_id",
-                Axis::new(Linear::new(0.0, 100.0), axis::Position::Right),
-            );
+        let mut chart_state = State::new();
 
+        // TODO: Add runtime grid on/off for axis
         // Initialize axes 0-100 on both axis
-        chart_state.set_axis(
-            X_ID,
-            Axis::new(Linear::new(AXIS_MIN, AXIS_MAX), axis::Position::Bottom),
-        );
-        chart_state.set_axis(
-            Y_ID,
-            Axis::new(Linear::new(AXIS_MIN, AXIS_MAX), axis::Position::Right),
-        );
-        (
-            Self {
-                chart_state,
-                star_system: SolarSystem::new(generate_values(100), 5.0),
-            },
-            Task::none(),
-        )
+        let x_axis = Axis::new(Linear::new(AXIS_MIN, AXIS_MAX), axis::Position::Bottom)
+            .with_grid_renderer(|ctx| {
+                // Very simple gridline implementation currently.
+                let major_gridline = GridLine {
+                    thickness: 1.into(),
+                };
+                let minor_gridline = GridLine {
+                    thickness: 0.5.into(),
+                };
+                match ctx.level {
+                    0 => Some(major_gridline),
+                    _ => Some(minor_gridline),
+                }
+            })
+            .with_custom_label_policy(|ctx| {
+                // Here you can use the context that you are given to determine if a specific tick should be rendered
+                // This feature is for those the want ultimate control. Its not necessary for most usecases
+                //
+                // Example of making different decisions:
+                let decision = axis::LabelDecision::Render;
+                decision
+            })
+            .with_cursor_formatter(|ctx| {
+                let cursor_label = iced_aksel::axis::Label {
+                    size: 32.into(),
+                    content: "Test".to_string(),
+                };
+                Some(cursor_label)
+            })
+            .with_tick_renderer(|ctx| {
+                // Here you get all the info you would need to determine which labels should show
+                // And which should show in different ways.
+                let level = ctx.tick.level;
+
+                let major_label = iced_aksel::axis::Label {
+                    size: 10.into(),
+                    content: "Major".to_string(),
+                };
+                let minor_label = iced_aksel::axis::Label {
+                    size: 6.into(),
+                    content: "Minor".to_string(),
+                };
+
+                let tick_line_major = iced_aksel::axis::TickLine {
+                    label: Some(major_label),
+                    length: 4.into(),
+                    thickness: 1.into(),
+                };
+                let tick_line_minor = iced_aksel::axis::TickLine {
+                    label: Some(minor_label),
+                    length: 2.into(),
+                    thickness: 0.5.into(),
+                };
+
+                match level {
+                    0 => Some(tick_line_major),
+                    1 => Some(tick_line_minor),
+                    _ => None,
+                }
+            })
+            .with_thickness(32.)
+            .with_label_spacing(0.0)
+            .skip_overlapping_labels(6.);
+        let y_axis = Axis::new(Linear::new(AXIS_MIN, AXIS_MAX), axis::Position::Right);
+
+        chart_state.set_axis(X_ID, x_axis);
+        chart_state.set_axis(Y_ID, y_axis);
+
+        (Self { chart_state }, Task::none())
     }
 
     fn update(&mut self, _message: Message) -> Task<Message> {
@@ -64,9 +114,7 @@ impl ExampleApp {
     }
 
     fn view(&self) -> Element<'_, Message> {
-        let chart = Chart::new(&self.chart_state)
-            .plot_data(&self.star_system, X_ID, Y_ID)
-            .on_drag(Message::ChartDragged);
+        let chart = Chart::new(&self.chart_state);
         chart.into()
     }
 
@@ -75,66 +123,4 @@ impl ExampleApp {
             .theme(Theme::Dark)
             .run()
     }
-}
-
-struct SolarSystem {
-    points: Vec<PlotPoint>,
-    star_size: f64,
-}
-
-impl SolarSystem {
-    const fn new(points: Vec<PlotPoint>, star_size: f64) -> Self {
-        Self { points, star_size }
-    }
-}
-
-impl PlotData<f64> for SolarSystem {
-    fn draw(&self, plot: &mut iced_aksel::Plot<f64, iced::Renderer>, _theme: &iced::Theme) {
-        // Prepare values
-        let bounds = plot.bounds();
-
-        for point in &self.points {
-            // Create 2D rect for culling
-            // let rect = PlotRect::from_center(*point, self.star_size, self.star_size);
-
-            // Cull data out of visible range
-            // if !bounds.intersects(&rect) {
-            //     continue;
-            // }
-
-            // Create shape
-            let geometry = star_geometry(point, self.star_size);
-            let shape = Polygon::new(geometry).fill(Color::WHITE);
-
-            plot.add_shape(shape);
-        }
-    }
-}
-
-/// Creates a 5 pointed star from this center with size
-fn star_geometry(point: &PlotPoint, size: f64) -> Vec<PlotPoint> {
-    let mut points = Vec::new();
-    let angle = 2.0 * PI / 5.0;
-    let mut angle_offset = 0.0;
-
-    for _ in 0..5 {
-        let x = (size * angle.cos()).mul_add(angle_offset, point.x);
-        let y = (size * angle.sin()).mul_add(angle_offset, point.y);
-
-        points.push(PlotPoint::new(x, y));
-
-        angle_offset += angle;
-    }
-
-    points
-}
-
-fn generate_values(amount: usize) -> Vec<PlotPoint> {
-    let mut rng = rand::rng();
-    (0..amount)
-        .map(|_| PlotPoint {
-            x: rng.random_range(AXIS_MIN..=AXIS_MAX),
-            y: rng.random_range(AXIS_MIN..=AXIS_MAX),
-        })
-        .collect()
 }
